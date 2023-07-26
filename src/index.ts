b6734449fba5bbd5f6ba983992976714373f4ae5
ridbay <balogunridwan@gmail.com>
2023-07-26 17:20:37 +0100

    made some changes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import User from './user';
 import Wallets from './wallets';
-import { UserType, WalletsType } from './types/index';
+import { UserType, WalletsType } from '../index.d';
 
 class Quidax {
   user: UserType;
